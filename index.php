ab2e898258ffa1e6eb5f13d79e2109c7c9df51c3
Nicolas TRIMARDEAU <nicolas@trimardeau.com>
2018-12-06 05:59:14 +0100

    301 No bot

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+	// Config
+	$redirectTo = 'https://www.trimardeau.com/';
+	// On récupère la liste des bots connus
+	$botListRaw = file_get_contents('./botlist.txt');
+	$botList = explode("\n",$botListRaw);
+	$userAgent = $_SERVER['HTTP_USER_AGENT'];
+	// On vérifie si l'User Agent est dans la liste des bots
+	$redirect = true;
+	foreach($botList as $bot){
+		if(preg_match("#".$bot."#isU", $userAgent)){
+			$redirect = false;
+			break;
+		}
+	}
+	// Si ce n'est pas un bot ou si c'est Google Bot, on redirige
+	if($redirect){
+		header("Location: $redirectTo",true, 301);
+	}
