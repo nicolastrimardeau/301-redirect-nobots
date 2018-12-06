2430d06ad8e9c799b8141e9d7b3490d033c01bec
Nicolas TRIMARDEAU <nicolas@trimardeau.com>
2018-12-06 19:23:48 +0100

    Fichier de configuration et différents modes

--- README.md
+++ README.md
@@ -4,7 +4,56 @@ Script permettant de mettre en place une redirection 301 vers un nouveau domaine
 # Utilisation
 - Importez le script sur votre serveur FTP.
 - Assurez-vous que le mod_rewrite d'apache est bien activé.
-- Modifier le site vers lequel vous souhaitez effectuer la redirection (ligne 4 du fichier index.php)
+- Modifier le fichier de configuration selon vos besoins
+
+# Configuration
+Pour modifier la configuration du script, utilisez le fichier config.json
+
+## redirectTo
+Indiquez le site vers lequel vous souhaitez envoyer le trafic
+
+## mod
+Plusieurs modes de cloacking sont à votre disposition.
+
+### UA-BCC (User-Agent Block Common Crawler)
+Bloc les crawler des outils d'analyse SEO tel que Majestic, SEMRush, AHref en fonction de l'User-Agent utilisé par le crawler. La liste des User-Agent est disponible et éditable dans le fichier ./d/blocked_ua.txt (utilise une expression régulière et non pas une comparaison exacte)
+
+```
+{
+	"redirectTo": "https://www.domain.tld",
+	"mod": "UA-BCC"
+}
+```
+
+### UA-AC (User-Agent Allow Crawler)
+Ne génère une redirection que pour les User-Agent autorisés dans le fichier authorized_ua.txt (utilise une expression régulière et non pas une comparaison exacte)
+
+```
+{
+	"redirectTo": "https://www.domain.tld",
+	"mod": "UA-AC"
+}
+```
+
+### IP-AC (IP Allow Crawler)
+Ne génère une redirection que pour les IP indiquées dans le fichier authorized_ip.txt (utilise une comparaison exacte)
+
+```
+{
+	"redirectTo": "https://www.domain.tld",
+	"mod": "IP-AC"
+}
+```
+
+### RDNS-AC (Reverse DNS Authorized Crawler)
+Ne génère une redirection que pour les crawler dont le nom d'hôte est dans la liste autorisée dans le fichier ./d/authorized_host.txt (utilise une expression régulière et non pas une comparaison exacte)
+
+```
+{
+	"redirectTo": "https://www.domain.tld",
+	"mod": "RDNS-AC"
+}
+```
 
 # Plus d'information
 Vous souhaitez mettre en place une stratégie de référencement naturel ? Faites appel à mes services de [consultant seo](https://www.trimardeau.com/)
--- index.php
+++ index.php
@@ -1,20 +1,85 @@
 <?php 
-
-	// Config
-	$redirectTo = 'https://www.yourdomain.tld/';
-	// On récupère la liste des bots connus
-	$botListRaw = file_get_contents('./botlist.txt');
-	$botList = explode("\n",$botListRaw);
-	$userAgent = $_SERVER['HTTP_USER_AGENT'];
-	// On vérifie si l'User Agent est dans la liste des bots
-	$redirect = true;
-	foreach($botList as $bot){
-		if(preg_match("#".$bot."#isU", $userAgent)){
-			$redirect = false;
-			break;
+	
+	$params = json_decode(file_get_contents('./config.json'));
+	// Récupération de la configuration
+	$redirectTo = $params->redirectTo;
+	// --- Block Common Crawlers (Bloc les crawlers connus (AHref, Majestic...))
+	if($params->mod == 'UA-BCC'){
+		// On récupère la liste des bots connus
+		$botListRaw = file_get_contents('./d/blocked_ua.txt');
+		$botList = explode("\n",$botListRaw);
+		$userAgent = $_SERVER['HTTP_USER_AGENT'];
+		// On vérifie si l'User Agent est dans la liste des bots
+		$redirect = true;
+		foreach($botList as $bot){
+			if(preg_match("#".$bot."#isU", $userAgent)){
+				$redirect = false;
+				break;
+			}
+		}
+		// Si ce n'est pas un bot ou si c'est Google Bot, on redirige
+		if($redirect){
+			header("Location: $redirectTo",true, 301);
 		}
 	}
-	// Si ce n'est pas un bot ou si c'est Google Bot, on redirige
-	if($redirect){
-		header("Location: $redirectTo",true, 301);
+	// --- Only Authorized Crawlers (Autorise uniquement certains User Agent)
+	if($params->mod == 'UA-AC'){
+		// On récupère la liste des bots autorisés
+		$botListRaw = file_get_contents('./d/authorized_ua.txt');
+		$botList = explode("\n",$botListRaw);
+		$userAgent = $_SERVER['HTTP_USER_AGENT'];
+		// On vérifie si l'User Agent est dans la liste des bots
+		$redirect = false;
+		foreach($botList as $bot){
+			if(preg_match("#".$bot."#isU", $userAgent)){
+				$redirect = true;
+				break;
+			}
+		}
+		// Si ce n'est pas un bot ou si c'est Google Bot, on redirige
+		if($redirect){
+			header("Location: $redirectTo",true, 301);
+		}
 	}
+	// --- Autorise uniquement certaines IP
+	if($params->mod == 'IP-AC'){
+		// On récupère la liste des bots autorisés
+		$addrListRaw = file_get_contents('./d/authorized_ip.txt');
+		$addrList = explode("\n",$addrListRaw);
+		$remoteAddr = $_SERVER['REMOTE_ADDR'];
+		var_dump($remoteAddr);
+		// On vérifie si l'User Agent est dans la liste des bots
+		$redirect = false;
+		foreach($addrList as $addr){
+			if($remoteAddr == $addr){
+				$redirect = true;
+				break;
+			}
+		}
+		// Si ce n'est pas un bot ou si c'est Google Bot, on redirige
+		if($redirect){
+			header("Location: $redirectTo",true, 301);
+		}
+	}
+	// --- Autorise uniquement les reverses DNS de la liste
+	if($params->mod == 'RDNS-AC'){
+		$dnsListRaw = file_get_contents('./d/authorized_host.txt');
+		$dnsList = explode("\n",$dnsListRaw);
+		$remoteAddr =  $_SERVER['REMOTE_ADDR'];
+		$host = gethostbyaddr($remoteAddr);
+		$ipFromHost = gethostbyname($host);
+		// Les ip ne correspondent pas, on ne redirige pas
+		if($remoteAddr != $ipFromHost){ die(); }
+		// On vérifie si le DNS match la liste de redirection
+		$redirect = false;
+		foreach($dnsList as $dns){
+			if(preg_match("#".$dns."#isU", $host)){
+				$redirect = true;
+				break;
+			}
+		}
+		if($redirect){
+			header("Location: $redirectTo",true, 301);
+		}
+	}
+	
